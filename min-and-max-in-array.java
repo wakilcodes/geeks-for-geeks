485441450c92fa718b77911005990f2e7f3c7d9a
wakilcodes <iamwakil.official@gmail.com>
2026-09-22 22:45:43 +0530

    Add getMinMax method to find min and max values
    Implement method to find minimum and maximum in an array.

---
+++ min-and-max-in-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public ArrayList<Integer> getMinMax(int[] arr) {
+        // code Here
+
+       
+        int min = arr[0];
+        int max = arr[0];
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+            min = arr[i];
+            }
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+         ArrayList<Integer> ans = new ArrayList<>();
+
+         ans.add(min);
+         ans.add(max);
+
+         return ans;
+       
+    }
+}
+
+
+          
+ 
